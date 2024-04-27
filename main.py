c474b900020960b28850ca10c09ab01fe30550d3
Gibril <gibrilissakaiddrisu@gmail.com>
2024-04-27 11:43:52 +0100

    cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.llm_routes import llm_routes
 
 # init app
@@ -6,3 +7,20 @@ app = FastAPI()
 
 # register routes
 app.include_router(llm_routes)
+
+
+# add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def root():
+    return {
+        'message': 'Server is working and alive 🥰.',
+        'shoutouts': 'Shout outs to Isaac Bcwin 🥰'
+    }
